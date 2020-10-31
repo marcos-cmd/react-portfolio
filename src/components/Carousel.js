1c37d729ead257e9119c8cfc1f6b52bdb0d65c69
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-10-31 16:45:15 -0700

    begin Carousel componenent

---
+++ src/components/Carousel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {                
+        }
+    }
+
+    render() {
+        return (
+            <p>Insert Carousel here!</p>
+        );
+    }
+}
+
+export default Carousel;
