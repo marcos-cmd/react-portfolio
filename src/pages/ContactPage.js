0abe12377f3b5a52415c55fd9b83ce76d61c30c9
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-11-17 11:33:33 -0800

    added light borders to contact cards

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -14,53 +14,53 @@ function AboutPage(props) {
         <div>
             <Hero title={props.title} />
             <Container fluid>
-            <CardDeck>
-                <Card>
-                    <Card.Body>
-                        <Card.Title className="c-card-title">Send Me an Email</Card.Title>
-                        <Button variant="info" size="lg" href="mailto:marcos.joseph.garcia@gmail.com" block><FontAwesomeIcon icon={faEnvelopeOpenText} size="5x" className="card-icon" /></Button>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Body>
-                        <Card.Title className="c-card-title">Follow me on Github</Card.Title>
-                        <Button variant="info" size="lg" target='_blank' href="https://github.com/marcos-cmd" block><FontAwesomeIcon icon={faGithub} size="5x" className="card-icon" /></Button>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Body>
-                        <Card.Title className="c-card-title">Connect with me on LinkedIn</Card.Title>
-                        <Button variant="info" size="lg" target='_blank' href="https://www.linkedin.com/in/marcosjgarcia/" block><FontAwesomeIcon icon={faLinkedin} size="5x" className="card-icon" /></Button>
-                    </Card.Body>
-                </Card>
-            </CardDeck>
-            <CardDeck className="lowerDeck">
-                <Card>
-                    <Card.Body>
-                        <Card.Title className="c-card-title">View my Resume</Card.Title>
-                        
-                        <Button variant="info" size="lg" href="mailto:marcos.joseph.garcia@gmail.com" block><FontAwesomeIcon icon={faFileDownload} size="5x" className="card-icon" /></Button>
-                    </Card.Body>
-                </Card>
-                <Card border="light">
-                    <blockquote className="blockquote mb-0 card-body">
-                        <p>
-                            It is true that the more one learns the less one knows.
+                <CardDeck>
+                    <Card border="light">
+                        <Card.Body>
+                            <Card.Title className="c-card-title">Send Me an Email</Card.Title>
+                            <Button variant="info" size="lg" href="mailto:marcos.joseph.garcia@gmail.com" block><FontAwesomeIcon icon={faEnvelopeOpenText} size="5x" className="card-icon" /></Button>
+                        </Card.Body>
+                    </Card>
+                    <Card border="light">
+                        <Card.Body>
+                            <Card.Title className="c-card-title">Follow me on Github</Card.Title>
+                            <Button variant="info" size="lg" target='_blank' href="https://github.com/marcos-cmd" block><FontAwesomeIcon icon={faGithub} size="5x" className="card-icon" /></Button>
+                        </Card.Body>
+                    </Card>
+                    <Card border="light">
+                        <Card.Body>
+                            <Card.Title className="c-card-title">Connect with me on LinkedIn</Card.Title>
+                            <Button variant="info" size="lg" target='_blank' href="https://www.linkedin.com/in/marcosjgarcia/" block><FontAwesomeIcon icon={faLinkedin} size="5x" className="card-icon" /></Button>
+                        </Card.Body>
+                    </Card>
+                </CardDeck>
+                <CardDeck className="lowerDeck">
+                    <Card border="light">
+                        <Card.Body>
+                            <Card.Title className="c-card-title">View my Resume</Card.Title>
+
+                            <Button variant="info" size="lg" href="mailto:marcos.joseph.garcia@gmail.com" block><FontAwesomeIcon icon={faFileDownload} size="5x" className="card-icon" /></Button>
+                        </Card.Body>
+                    </Card>
+                    <Card border="light">
+                        <blockquote className="blockquote mb-0 card-body">
+                            <p>
+                                It is true that the more one learns the less one knows.
                         </p>
-                        <footer className="blockquote-footer">
-                            <small className="text-muted">
-                                James Baldwin in <cite target='_blank' href="https://www.theparisreview.org/interviews/2994/the-art-of-fiction-no-78-james-baldwin">The Paris Review</cite>
-                            </small>
-                        </footer>
-                    </blockquote>
-                </Card>
-                <Card>
-                    <Card.Body>
-                        <Card.Title className="c-card-title">Follow me on Medium</Card.Title>
-                        <Button variant="info" size="lg" target='_blank' href="https://medium.com/@marcos-cmd" block><FontAwesomeIcon icon={faMedium} size="5x" className="card-icon" /></Button>
-                    </Card.Body>
-                </Card>
-            </CardDeck>
+                            <footer className="blockquote-footer">
+                                <small className="text-muted">
+                                    James Baldwin in <cite target='_blank' href="https://www.theparisreview.org/interviews/2994/the-art-of-fiction-no-78-james-baldwin">The Paris Review</cite>
+                                </small>
+                            </footer>
+                        </blockquote>
+                    </Card>
+                    <Card border="light">
+                        <Card.Body>
+                            <Card.Title className="c-card-title">Follow me on Medium</Card.Title>
+                            <Button variant="info" size="lg" target='_blank' href="https://medium.com/@marcos-cmd" block><FontAwesomeIcon icon={faMedium} size="5x" className="card-icon" /></Button>
+                        </Card.Body>
+                    </Card>
+                </CardDeck>
             </Container>
             <Hero />
         </div>
