16237522cf3e013f57932c33c7b01a5114a7acc6
marcos-cmd <marcos.joseph.garcia@gmail.com>
2021-03-19 23:14:00 -0700

    added animation to Homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from '../components/Hero';
-import Carousel from '../components/Carousel';
+import { init } from 'ityped';
 
+import './HomePage.css';
 
 
 function HomePage(props) {
 
+    useEffect(() => {
+        const type = document.querySelector('#type')
+        // init(type, { showCursor: false, typeSpeed: 100, backSpeed: 45, strings: [':-)', 'Hello There!', 'Welcome to my site!', 'Learn about who I am...', 'Send me a message...', 'Check out my work!'] })
+        init(type, { showCursor: false, typeSpeed: 100, backSpeed: 45, strings: ["You're looking at the website of a...", "Designer", "Developer", "Educator", "and Clown 🤡", "How does that make you feel?", "Let me know 😜", "Why are you still here?", "Check out my portfolio!"] })
+
+    });
+       
+    
     return(
        <div>
-            <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
+            <Hero title={props.title}  text={props.text} />
+                    <div class="subtitle" id="type"></div>
             <Hero />
             <Hero />
             <Hero />
